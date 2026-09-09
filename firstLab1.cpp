5d6dad6cd4492510d41f21766d36a65e9105489d
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2026-09-09 15:11:49 +0300

    save

--- firstLab1.cpp
+++ firstLab1.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello, Worldwaewaedwa!" << std::endl;
-    return 0;
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    // Завдання 1
+    double A, B, C, X;
+
+    cout << "Введіть A: ";
+    cin >> A;
+
+    cout << "Введіть B: ";
+    cin >> B;
+
+    cout << "Введіть C: ";
+    cin >> C;
+
+    X = pow(sin(C), 3) * pow(cos(A), 2) / (exp(A) + 1 + sin(B));
+
+    cout << "X = " << X << endl;
+
+    // ЗАвдання 2
+    double P, d;
+
+    cout << "Введіть периметер P: ";
+    cin >> P;
+
+    d = P / 4;
+
+    cout << "Найменша діагональ = " << d << endl;
+
+    return 1;
 }
